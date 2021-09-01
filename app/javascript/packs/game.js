0117b10fefe2982cf3437b15a2705767b61f20f5
Bruno Santiago <br.gabriel10@gmail.com>
2021-09-01 22:31:33 +0200

    game

---
+++ app/javascript/packs/game.js
@@ -0,0 +1,194 @@
+const gameJs = () => {
+
+  const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: "game-container",
+    pixelArt: true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 0 }
+      }
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update
+    }
+  };
+
+  const game = new Phaser.Game(config);
+  let cursors;
+  let player;
+  let showDebug = false;
+
+  function preload() {
+    this.load.image("tiles", "./app/assets/game/world.png");
+    this.load.tilemapTiledJSON("map", "./world.json");
+    this.load.atlas("atlas", "./app/assets/game/ruby.png", "./app/assets/game/ruby.json");
+  }
+
+  function create() {
+    const map = this.make.tilemap({ key: "map" });
+
+    // Parameters are the name you gave the tileset in Tiled and then the key of the tileset image in
+    // Phaser's cache (i.e. the name you used in preload)
+    const tileset = map.addTilesetImage("world", "tiles");
+
+    // Parameters: layer name (or index) from Tiled, tileset, x, y
+    const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+    const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+    const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+
+    worldLayer.setCollisionByProperty({ collides: true });
+
+    // By default, everything gets depth sorted on the screen in the order we created things. Here, we
+    // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
+    // Higher depths will sit on top of lower depth objects.
+    aboveLayer.setDepth(10);
+
+    // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
+    // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
+    const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+    // Create a sprite with physics enabled via the physics system. The image used for the sprite has
+    // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
+    player = this.physics.add
+      .sprite(spawnPoint.x, spawnPoint.y, "atlas", "ruby-down-walk.000")
+      .setSize(30, 40)
+      .setOffset(0, 0);
+
+    // Watch the player and worldLayer for collisions, for the duration of the scene:
+    this.physics.add.collider(player, worldLayer);
+
+    // Create the player's walking animations from the texture atlas. These are stored in the global
+    // animation manager so any sprite can access them.
+    const anims = this.anims;
+    anims.create({
+      key: "ruby-left-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "ruby-left-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    anims.create({
+      key: "ruby-right-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "ruby-right-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    anims.create({
+      key: "ruby-down-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "ruby-down-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+    anims.create({
+      key: "ruby-up-walk",
+      frames: anims.generateFrameNames("atlas", {
+        prefix: "ruby-up-walk.",
+        start: 0,
+        end: 3,
+        zeroPad: 3
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    const camera = this.cameras.main;
+    camera.startFollow(player);
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    // Help text that has a "fixed" position on the screen
+    this.add
+      .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+        font: "18px monospace",
+        fill: "#000000",
+        padding: { x: 20, y: 10 },
+        backgroundColor: "#ffffff"
+      })
+      .setScrollFactor(0)
+      .setDepth(30);
+
+    // Debug graphics
+    this.input.keyboard.once("keydown_D", event => {
+      // Turn on physics debugging to show player's hitbox
+      this.physics.world.createDebugGraphic();
+
+      // Create worldLayer collision graphic above the player, but below the help text
+      const graphics = this.add
+        .graphics()
+        .setAlpha(0.75)
+        .setDepth(20);
+      worldLayer.renderDebug(graphics, {
+        tileColor: null, // Color of non-colliding tiles
+        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+      });
+    });
+  }
+
+  function update(time, delta) {
+    const speed = 175;
+    const prevVelocity = player.body.velocity.clone();
+
+    // Stop any previous movement from the last frame
+    player.body.setVelocity(0);
+
+    // Horizontal movement
+    if (cursors.left.isDown) {
+      player.body.setVelocityX(-speed);
+    } else if (cursors.right.isDown) {
+      player.body.setVelocityX(speed);
+    }
+
+    // Vertical movement
+    if (cursors.up.isDown) {
+      player.body.setVelocityY(-speed);
+    } else if (cursors.down.isDown) {
+      player.body.setVelocityY(speed);
+    }
+
+    // Normalize and scale the velocity so that player can't move faster along a diagonal
+    player.body.velocity.normalize().scale(speed);
+
+    // Update the animation last and give left/right animations precedence over up/down animations
+    if (cursors.left.isDown) {
+      player.anims.play("ruby-left-walk", true);
+    } else if (cursors.right.isDown) {
+      player.anims.play("ruby-right-walk", true);
+    } else if (cursors.up.isDown) {
+      player.anims.play("ruby-up-walk", true);
+    } else if (cursors.down.isDown) {
+      player.anims.play("ruby-down-walk", true);
+    } else {
+      player.anims.stop();
+
+      // If we were moving, pick and idle frame to use
+      if (prevVelocity.x < 0) player.setTexture("atlas", "ruby-left-walk.000");
+      else if (prevVelocity.x > 0) player.setTexture("atlas", "ruby-right-walk.000");
+      else if (prevVelocity.y < 0) player.setTexture("atlas", "ruby-up-walk.000");
+      else if (prevVelocity.y > 0) player.setTexture("atlas", "ruby-down-walk.000");
+    }
+  }
+};
+
+export { gameJs }
