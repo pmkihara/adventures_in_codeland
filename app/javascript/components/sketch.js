3e6560ff8830711b6dac64f4ed448200312ce757
Patricia Kihara <patricia.kihara@outlook.com>
2021-09-01 12:03:45 -0300

    Character and map placed

--- app/javascript/components/sketch.js
+++ app/javascript/components/sketch.js
@@ -11,10 +11,19 @@ const initP5 = () => {
       const map = s.createDiv();
       map.addClass('map pixel-art');
       map.style("background-image: url(/assets/map-05df230a8d7156c2802186ddd4486efe0accc75a3a3627c5966b72bcc5f8615c.png)");
+      map.parent("sketch")
 
       const character = s.createDiv();
-      character.addClass('pixel-art character-sprite character-down');
-      character.style("background-image: url(/assets/cat-sprite-3e3fbf4062b56d456f56c73f3b7d58bcef2b90f8c2d5a6bea565fa64173e1592.png)");
+      character.addClass('character')
+      character.id('character')
+      character.parent("sketch")
+      character.position(415,300)
+
+      const characterSprite = s.createDiv();
+      characterSprite.addClass('pixel-art character-sprite character-down');
+      characterSprite.style("background-image: url(/assets/cat-sprite-3e3fbf4062b56d456f56c73f3b7d58bcef2b90f8c2d5a6bea565fa64173e1592.png)");
+      characterSprite.parent("character")
+
     }
 
     s.draw = () => {
