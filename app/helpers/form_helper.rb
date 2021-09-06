2bc0713dd67be5d4c72a425846d467dfa47becc6
Bruno Santiago <br.gabriel10@gmail.com>
2021-09-06 16:58:31 +0200

    stimulus function

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,14 +1,4 @@
 module FormHelper
-  def stimulus_function
-    if current_user.name.nil?
-      # caminho para atualizar o nome
-    elsif current_user.age.nil?
-      # caminho para atualizar a idade
-    else
-      "validateAnswer"
-    end
-  end
-
   def choose_path
     if current_user.name.nil?
       # caminho para atualizar o nome
@@ -16,6 +6,16 @@ module FormHelper
       # caminho para atualizar a idade
     else
       validate_answer_path
+    end
+  end
+
+  def stimulus_function
+    if current_user.name.nil?
+      "validateName"
+    elsif current_user.age.nil?
+      "validateAge"
+    else
+      "validateAnswer"
     end
   end
 end
