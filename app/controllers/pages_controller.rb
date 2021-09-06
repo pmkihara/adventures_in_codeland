60008f31c8c40d24d4168c589b65a94720f2d146
Felipe von Brasche Figueiredo <felipe@vonbrasche.com>
2021-09-06 23:48:14 +0200

    score table visible witout login and colors changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home]
+  skip_before_action :authenticate_user!, only: %i[home story score]
   def home
   end
 
