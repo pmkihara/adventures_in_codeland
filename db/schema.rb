ff97a2743d27d78691e46475719c7f7d07f6b526
Bruno Santiago <br.gabriel10@gmail.com>
2021-09-06 19:26:00 +0200

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -63,8 +63,8 @@ ActiveRecord::Schema.define(version: 2021_09_06_132410) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "age"
+    t.string "name", null: false
+    t.integer "age", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
