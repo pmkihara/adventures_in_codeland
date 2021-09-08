773b487b34008d76e1bdb7affae4c4372c46d61a
Igor Depolli <igorcler@gmail.com>
2021-09-08 15:16:37 -0300

    Name to user added and configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   include Pundit
 
@@ -17,5 +18,13 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not allowed to play this game!"
     redirect_to(request.referrer || root_path)
+  end
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
